d2fd10712ef6680f4ca4406d7cf92870c5343a95
marupon3910 <96562149+marupon3910@users.noreply.github.com>
2022-04-19 18:35:18 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package forcase1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 0;
+		for (i = 0; i < 30; i++) {
+			if (i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
